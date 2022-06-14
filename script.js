8154d6bae38258e87d628991d0bca02bec9f71e9
EmVale <107022976+EmVale@users.noreply.github.com>
2022-06-13 21:09:05 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'rgb(0, 0, 0)';
 ctx.fillRect(0, 0, width, height);
+
+ctx.fillStyle = 'rgb(41, 140, 97)';
+ctx.fillRect(width * 0.1, height * 0.1, width * 0.8, height * 0.8);
+
+function degToRad(degrees){
+    return degrees * Math.PI / 180;
+}
+
+ctx.fillStyle = 'rgb(41, 140, 97)';
+ctx.font = '60px arial';
+var txt = 'Amazing Game';
+ctx.fillText('Amazing Game', width / 2 - ctx.measureText(txt).width / 2, height * 0.1 / 2 + 20);
+
+const image = new Image();
+image.src = 'testimage.jpg';
+image.addEventListener('load', () => ctx.drawImage(image, width / 2 - image.naturalWidth / 4, height / 2 - image.naturalHeight / 4, 960, 540));
+
+
+
