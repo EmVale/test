c8b1ee3cae836de506014f1e53b292ca5a7a47a9
EmVale <107022976+EmVale@users.noreply.github.com>
2022-07-21 04:23:22 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,113 +1,123 @@
+//initial setup
 const canvas = document.querySelector('.myCanvas');
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+//const width = canvas.width = window.innerWidth;
+//const height = canvas.height = window.innerHeight;
+const width = canvas.width = 1200;
+const height = canvas.height = 675;
 const ctx = canvas.getContext('2d');
-const button = document.querySelector('button');
-var pressed = false;
+var betsize = 1;
+var spinning = 0;
 
-ctx.fillStyle = 'rgb(0, 0, 0)';
-ctx.fillRect(0, 0, width, height);
+//dictionary of bet sizes
+const betsizes = {1:0.10, 2:0.20, 3:0.40, 4:0.60, 5:0.80, 6:1.00, 7:1.20, 8:1.40, 9:1.60, 10:1.80, 11:2.00, 12:3.00, 13:4.00, 14:5.00, 15:6.00, 16:7.00, 17:8.00, 18:9.00, 19:10.00, 20:15.00, 21:20.00, 22:25.00, 23:50.00, 24:75.00, 25:100.00};
 
-ctx.fillStyle = 'rgb(41, 140, 97)';
-ctx.fillRect(width * 0.1, height * 0.1, width * 0.8, height * 0.8);
+//boxes for buttons
+const increasebox = {x:width*0.9, y:height*0.85, width:width*0.1, height:height*0.15};
+const decreasebox = {x:width*0.65, y:height*0.85, width:width*0.1, height:height*0.15};
+const menubox = {x:0, y:height*0.85, width:width*0.1, height:height*0.15};
+const spinbox = {x:width*0.85, y:height*0.65, width:width*0.1, height:height*0.15};
 
-function degToRad(degrees){
-    return degrees * Math.PI / 180;
-}
+//main function to draw the outer board
+function drawframe(){
 
-ctx.fillStyle = 'rgb(41, 140, 97)';
-ctx.font = '60px arial';
-var txt = 'Amazing Game';
-var bttxt = 'Wow what a great button!';
-ctx.fillText('Amazing Game', width / 2 - ctx.measureText(txt).width / 2, height * 0.1 / 2 + 20);
-
-const image = new Image();
-image.src = 'testimage.jpg';
-image.addEventListener('load', () => ctx.drawImage(image, width / 2 - image.naturalWidth / 4, height / 2 - image.naturalHeight / 4, 960, 540));
-
-
-function rand(min, max) {
-    return Math.floor(Math.random() * (max-min+1)) + (min);
-}
-  
-ctx.translate(width/2, height/2);
-
-const walk = new Image();
-walk.src = 'walk-right.png';
-walk.onload = draw;
-let sprite = 0;
-let posX = 0;
-
-function draw() {
-    ctx.translate(-width/2, -height/2);
+    //primary background
     ctx.fillStyle = 'rgb(0, 0, 0)';
     ctx.fillRect(0, 0, width, height);
-    ctx.fillStyle = 'rgb(41, 140, 97)';
-    ctx.fillRect(width * 0.1, height * 0.1, width * 0.8, height * 0.8);
-    ctx.fillText('Amazing Game', width / 2 - ctx.measureText(txt).width / 2, height * 0.1 / 2 + 20);
-    ctx.drawImage(image, width / 2 - image.naturalWidth / 4, height / 2 - image.naturalHeight / 4, 960, 540);
-    if (pressed === true){
-        ctx.fillStyle = 'rgb(0, 0, 0)';
-        ctx.font = '60px arial';
-        ctx.fillText('Wow what a great button!', width / 2 - ctx.measureText(bttxt).width / 2, height * 0.17);
-    }
-    ctx.translate(width/2, height/2);
-    ctx.drawImage(walk, (sprite*102), 0, 102, 148, 0+posX, -74, 102, 148);
-    if (posX % 13 === 0) {
-        if (sprite === 5) {
-          sprite = 0;
-        } 
-        else {
-          sprite++;
-        }
-    }
-    if (posX % 13 === 0) {
-        if (sprite === 5) {
-          sprite = 0;
-        } 
-        else {
-          sprite++;
-        }
-    }
-    if(posX > width/2) {
-        let newStartPos = -((width/2) + 102);
-        posX = Math.ceil(newStartPos);
-        console.log(posX);
-      } else {
-        posX += 2;
-    }
-    window.requestAnimationFrame(draw);
+
+    //bottom menu bar background
+    ctx.fillStyle = 'rgb(105, 101, 93, 0.5)';
+    ctx.fillRect(0, height*0.85, width, height*0.15);
+
+    //bottom left options button
+    ctx.fillStyle = 'rgb(73, 123, 204, 0.5)';
+    ctx.fillRect(0, height*0.85, width*0.1, height*0.15);
+
+    //balance display
+    ctx.fillStyle = 'rgb(43, 117, 73, 0.5)';
+    ctx.fillRect(width*0.1, height*0.85, width*0.2, height*0.15);
+
+    //bet increase button
+    ctx.fillStyle = 'rgb(43, 117, 73, 0.5)';
+    ctx.fillRect(width*0.9, height*0.85, width*0.1, height*0.15);
+
+    //bet size display
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    var fontsize = height*0.045;
+    ctx.font = fontsize + 'px arial';
+    ctx.fillText('Bet Size:', width*0.75 + ((width*0.15 - ctx.measureText('Bet Size:').width)/2), height*0.91);
+    ctx.fillText('$' + betsizes[betsize], width*0.75 + ((width*0.15 - ctx.measureText('$' + betsizes[betsize]).width)/2), height*0.965);
+
+    //bet decrease button
+    ctx.fillStyle = 'rgb(184, 53, 86, 0.5)';
+    ctx.fillRect(width*0.65, height*0.85, width*0.1, height*0.15);
+
+    //spin button
+    ctx.fillStyle = 'rgb(43, 117, 73, 0.5)';
+    ctx.fillRect(width*0.85, height*0.65, width*0.1, height*0.15);
+
+    //test moving box
+    ctx.fillStyle = 'rgb(64, 84, 230)';
+    if (spinning === 0){
+        ctx.fillRect(0, height*0.4, width*0.1, height*0.15);
+    };
+    if (spinning === width*0.5){
+        ctx.fillRect(spinning, height*0.4, width*0.1, height*0.15);
+        spinning = 0;
+
+    };
+    if (spinning > 0){
+        ctx.fillRect(spinning, height*0.4, width*0.1, height*0.15);
+        spinning +=1;
+        window.requestAnimationFrame(drawframe);
+    };
+    
+};
+
+//function to check if position is inside box
+function isInside(pos, rect){
+    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y;
 }
 
-button.addEventListener('click', () => {
-    ctx.fillStyle = 'rgb(41, 140, 97)';
-    ctx.font = '60px arial';
-    ctx.fillText('Wow what a great button!', width / 2 - ctx.measureText(txt).width / 2, height * 0.1 / 2 + 20);
-    pressed = !pressed;
-});
-
+//function to get the position of the mouse
 function getMousePos(canvas, event) {
     var rect = canvas.getBoundingClientRect();
     return {
         x: event.clientX - rect.left,
         y: event.clientY - rect.top
     };
-}
+};
 
-function isInside(pos, rect){
-    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y;
-}
+//function to increase bet size
+function increasebet(){
+    if (betsize < 25){
+        betsize += 1;
+        drawframe();
+    }
+};
 
-var testrect = {
-    x:width * 0.1,   
-    y:height * 0.1,
-    width:width * 0.8,
-    height:height * 0.8
-}
+//function to decrease bet size
+function decreasebet(){
+    if(betsize >1){
+        betsize -= 1;
+        drawframe();
+    }
+};
 
+//event listener for clicks
 canvas.addEventListener('click', function(e) {
     var mousePos = getMousePos(canvas, e);
-    if (isInside(mousePos, testrect)){
-        pressed = !pressed;
-    }
+    if (spinning === 0){
+        if (isInside(mousePos, increasebox)){
+            increasebet();
+        }
+        if (isInside(mousePos, decreasebox)){
+            decreasebet();
+        }
+        if (isInside(mousePos, spinbox)){
+            spinning = 1;
+            drawframe();
+        }
+    };
 });
+
+drawframe();
